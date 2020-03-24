e3a410a50bdc67b89b052eeee1d002fd4fcbb1fb
alikunwar <alisha.kunwar@mavs.uta.edu>
2020-03-24 04:43:25 -0500

    first project

--- P05/full_credit/taxed.h
+++ P05/full_credit/taxed.h
@@ -1,18 +1,18 @@
 #ifndef _TAXED_H
 #define _TAXED_H
 
-#include "product.h"
+
 #include <iostream>
 #include <exception>
 #include <string>
 
-class Taxed: public Product
+class Taxed:virtual public Product
 {
 	public:
 	Taxed(std::string name, double cost);
 	virtual ~Taxed();
 	static void set_tax_rate(double sales_tax);
-	double price() const override;
+	virtual double price () const override;
 	
 	
 	private:
