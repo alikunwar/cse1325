59c71dc0a7d1430237f4ca8e2109911189cd00b8
alikunwar <alisha.kunwar@mavs.uta.edu>
2020-03-24 04:53:01 -0500

    P03

---
+++ P03/full_credit/color.cpp
@@ -0,0 +1,45 @@
+#include "color.h"
+
+  
+  Color::Color() // Declaring default constructor
+  :_reset{true}{}
+ 
+  Color::Color(int red, int green , int blue)    // Declaring Constructor
+        : _red{red}, _green{green}, _blue{blue},_reset{false}{
+		if( red<0 || red >256)throw std::runtime_error{"Invalid number"};
+		if( blue<0 || blue>256)throw std::runtime_error{"Invalid number"};
+		if( green<0 || green>256)throw std::runtime_error{"Invalid number"};
+		}
+  std:: string Color::to_string()             //Changing ints to string using to_string
+  {                 
+	   return  std::to_string(_red) + std::to_string(_green)  + std::to_string(_blue) ;
+  }
+
+std::ostream& operator<<(std::ostream& ost, const Color& color)
+{
+	if(color._reset == false)
+ {
+    ost << "\033[38;2;" + std::to_string(color._red) + ";" + std::to_string(color._green) + ";" + std::to_string(color._blue)+"m";
+    return ost;
+ }	
+    else
+ {
+	 
+    ost<< "\033[0m" ;
+    return ost;
+   
+ }
+  
+} 
+
+std::istream &operator>>(std::istream &ist, Color &color){
+		ist >> color._red ;
+		ist>>color._green;
+		ist>>color._blue; 
+		color._reset = false;
+		return ist;
+}
+
+	
+
+
