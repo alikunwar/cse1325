6722bdaa9f8b2953a156d216dbdd5fd3620e12e1
alikunwar <alisha.kunwar@mavs.uta.edu>
2020-04-07 07:24:36 -0500

    sprint3

--- elsa/sprint2/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "mainwin.h"
-
-int main (int argc, char *argv[])
- {
-    srand (time(NULL));
-    auto app = Gtk::Application::create(argc, argv, "edu.uta.cse1325.store.v1_2_1");
-    Mainwin win;
-    return app->run(win);
-}
